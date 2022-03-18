cf6b223ce2559de0d81384828917ce1205cd12b0
David Maricle <david.maricle@abbott.com>
2022-03-18 16:58:34 -0500

    Just installing the template.

---
+++ todo-list/src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Welcome to David's website!</h1>
+        <p>More to come!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
